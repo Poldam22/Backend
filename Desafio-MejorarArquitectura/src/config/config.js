e7ed0df7e03ab642e6398760ccd0d32b60f9c85b
Pablo Santos <pablo.da.santos@gmail.com>
2023-04-03 20:40:39 -0300

    Desafío Mejorar arquitectura

---
+++ Desafio-MejorarArquitectura/src/config/config.js
@@ -0,0 +1,10 @@
+
+export const config = {
+    mongodb:{
+        host: 'mongodb://localhost:27017/usuarios',
+        options:{
+            useNewUrlParser: true,
+            useUnifiedTopology:true
+        }
+    }
+}
